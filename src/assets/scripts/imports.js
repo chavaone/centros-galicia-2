d0b6d0e740e90a8bd7f1e7cc4dbbfa015dc2a225
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2018-05-10 22:03:42 +0200

    Add FontAwesome

--- src/assets/scripts/imports.js
+++ src/assets/scripts/imports.js
@@ -1,3 +1,12 @@
 
 //Bootstrap
 import 'bootstrap';
+
+//FontAwesome
+import fontawesome from '@fortawesome/fontawesome';
+import solid from '@fortawesome/fontawesome-free-solid';
+
+fontawesome.library.add(solid.faTrash);
+fontawesome.library.add(solid.faClipboard);
+fontawesome.library.add(solid.faInfo);
+fontawesome.library.add(solid.faCar);
