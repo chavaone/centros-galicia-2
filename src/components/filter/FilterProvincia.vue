c7ed2c2ea26b8b8b30dd5341c33ff335d00d2c76
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2018-05-10 23:53:47 +0200

    FilterProvincia implemented

---
+++ src/components/filter/FilterProvincia.vue
@@ -0,0 +1,47 @@
+<template lang="html">
+  <div class="filter">
+    <h5>{{ $t('provinces') }}</h5>
+    <div class="form-check" v-for="(provincia, key) in provincias">
+      <input type="checkbox" class="form-check-input" :value="provincia" :id="key + '-checkbox'" v-model="checkedProvincias" @change="">
+      <label class="form-check-label" :for="key + '-checkbox'">{{provincia}}</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data : function() {
+    return {
+      provincias: {
+        'acoruna': 'A Coruña',
+        'lugo': 'Lugo',
+        'ourense': 'Ourense',
+        'pontevedra': 'Pontevedra'
+      },
+      checkedProvincias: [
+        'A Coruña',
+        'Lugo',
+        'Ourense',
+        'Pontevedra'
+      ]
+    };
+  },
+  methods: {
+    filter(centro) {
+      return centro.provincia in this.checkedProvincias;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
+<i18n>
+  {
+    "gl": {
+      "provinces": "Provincias:"
+    }
+  }
+</i18n>
